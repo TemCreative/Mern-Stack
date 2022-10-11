844b31060f9ebb23797624a729b107a884459ccf
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-10-11 10:14:22 +0100

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+
+export const MyComponent = () => {
+    const myName = 'Akande Enoch'
+    const department = "Software Engineering"
+    const school = "Soft Quest Incorporated"
+}
